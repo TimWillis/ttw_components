f6ea7e24477a1717a8caece6eaa32d25570107bc
Tim Willis <mail@timtwillis.com>
2022-05-29 12:01:12 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * from './components/ddl';
-export * from './components/modal';
+import ddl from './components/ddl';
+import modal from './components/modal';
+
+export default { ddl, modal };
+// export ddl;
+// export ddl from './components/ddl';
